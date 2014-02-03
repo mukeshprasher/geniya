7dde8abffa27c0a692cb3c78ddeb26a3533efe4d
Neeraj Singh <neeraj@60degree.com>
2014-02-03 13:39:21 +0530

    Initialize repo with blank rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Geniya::Application.config.secret_key_base = '61519b3658a53da0d5133cb74692b16ed4dbf12e94f0cdfcdfe981a57ec3197f5d45f323ff73e3f060ee8948ea14373332a487bd36ffb2505346b1d10030edb5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Geniya::Application.config.session_store :cookie_store, key: '_geniya_session'
