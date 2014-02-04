bd3e74927340fd06c2c88452d474fb5e2127b1a4
Neeraj Singh <neeraj@60degree.com>
2014-02-04 15:20:53 +0530

    title for user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Geniya::Application.routes.draw do
   resources :users
+  match '/signup',  to: 'users#new',            via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
