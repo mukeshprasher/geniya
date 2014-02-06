977414087e0f4fb9c5b551d3e2f6e5f0cbbb8fe4
Neeraj Singh <neeraj@60degree.com>
2014-02-06 16:47:25 +0530

    showing update feed

--- app/models/update.rb
+++ app/models/update.rb
@@ -5,4 +5,13 @@ class Update < ActiveRecord::Base
   validates :sender_user_id, presence: true
   validates :receiver_user_id, presence: true
   default_scope -> { order('created_at DESC') }
+
+  def self.from_users_followed_by(user)
+    followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"
+    where("sender_user_id IN (#{followed_user_ids}) OR sender_user_id = :user_id",
+          user_id: user.id)
+  end
+
 end
+
