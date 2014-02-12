842fccbf4820caee15a3261170de7e0261c3a613
Neeraj Singh <neeraj.singh@60degree.com>
2014-02-12 13:41:21 +0530

    user connections relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,13 @@ class User < ActiveRecord::Base
     has_many :followers, through: :reverse_relationships, source: :follower
     has_many :user_sub_categories     
     has_many :sub_categories, :through => :user_sub_categories, :source => :sub_category
-    has_many :userconnections, through: :user_connections
+
+    has_many :user_connections
+    has_many :connections, through: :user_connections, source: :sender_user
+
+    has_many :sent_requests, class_name: "UserConnection", foreign_key: "sender_user_id"
+    has_many :reverse_connections, through: :sent_requests, source: :user
+    
     has_many :albums
 
     before_save { self.email = email.downcase }
--- app/models/user_connection.rb
+++ app/models/user_connection.rb
@@ -1,4 +1,4 @@
 class UserConnection < ActiveRecord::Base
-  belongs_to :sender_user, class_name: "User"
-  belongs_to :user, class_name: "User"
+  belongs_to :sender_user, class_name: "User", :foreign_key =>"sender_user_id"
+  belongs_to :user
 end
