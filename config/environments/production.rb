4473c9f7f98781f6a8c99a8e5427e258e7dadab4
Neeraj Singh <neeraj.singh@60degree.com>
2014-02-26 18:07:28 +0530

    production settings for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,17 @@ Geniya::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => "geniya.herokuapp.com" }
+  config.action_mailer.smtp_settings = {
+        :address => "smtp.gmail.com",
+        :port => 587,
+        :domain => 'localhost',
+        :user_name => 'neeraj.avant.garde@gmail.com',
+        :password => 'neerajpass12',
+        :authentication => :plain,
+        :enable_starttls_auto => true
+  }
 end
