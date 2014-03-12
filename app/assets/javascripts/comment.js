f030c8134da88ae3571f82e80b115b3284cf0554
mukesh-avant-garde <mukesh.prashar@60degree.com>
2014-03-12 17:37:14 +0530

    complete the reply-comments functionality on albums

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -6,13 +6,32 @@ $(function() {
       $("#child-comment-form-"+comment_id).show();
     });
     $(".comment-delete").click(function(){
+     var delcomment = confirm("Do you want to delete this comment ?");
+     if(delcomment == true)
+     {
      var delete_comment_id=this.id.split("-")[2]
-     $("#comment-"+delete_comment_id).hide();
-    });
+       $("#comment-"+delete_comment_id).hide();
+       return true;
+     }
+     else
+       {$("#comment-"+delete_comment_id).show();
+       return false;}
+     });
+    
     $(".child-comment-delete").click(function(){
-     var child_comment_delete_id=this.id.split("-")[3]
-     $("#child-comment-"+child_comment_delete_id).hide();
-    })
+      var delchildcomment = confirm("Do you want to delete this comment ?");
+      if(delchildcomment == true)
+      {
+        var child_comment_delete_id=this.id.split("-")[3]
+        $("#child-comment-"+child_comment_delete_id).hide();
+        return true;
+      }
+      else
+      {
+       $("#child-comment-"+child_comment_delete_id).show(); 
+       return false;
+      }
+    });
 
   }
   $(document).ready(toDoOnloadComment);
