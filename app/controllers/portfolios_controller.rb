f440f0c8c3d7430c50c4bed30ec06f1b8621c0b5
Neeraj Singh <neeraj.singh@60degree.com>
2014-04-16 19:52:07 +0530

    portfolio sub-category search

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,6 @@
 class PortfoliosController < ApplicationController
   def index
+    @search_categories = @categories
     if !params[:q].nil?
       q = "%#{params[:q]}%"
       
@@ -8,12 +9,26 @@ class PortfoliosController < ApplicationController
       @albums_by_description = Album.where("description like ? and kind=?", q, 'portfolio').order(impressions_count: :desc)
       
       @albums = @albums_by_name + @albums_by_title + @albums_by_description
+    elsif !params[:gr].nil?
+      @albums = Array.new
+      params[:gr].each do |group_id|
+        group = SubCategoryGroup.find(group_id.first)
+        group.sub_categories.each do |sub_category|
+          sub_category.users.each do |user|
+            user.albums.where(kind: 'portfolio').each do |album|
+              @albums << album
+            end
+          end
+        end
+      end
+      @albums.sort! { |a, b|  b.impressions_count <=> a.impressions_count }
     else
       @albums = Album.where(:kind => "portfolio").order(impressions_count: :desc)
     end
   end
   def category_index
     @category = Category.find(params[:id])
+    @search_categories = Category.where(id: @category.id)
 
     if !params[:q].nil?
       q = "%#{params[:q]}%"
@@ -23,6 +38,17 @@ class PortfoliosController < ApplicationController
       @albums_by_description = Album.where("category_id = ? and description like ? and kind=?", @category.id, q, 'portfolio').order(impressions_count: :desc)
       
       @albums = @albums_by_name + @albums_by_title + @albums_by_description
+    elsif !params[:sc].nil?
+      @albums = Array.new
+      params[:sc].each do |sc_id|
+        sub_category = SubCategory.find(sc_id.first)
+        sub_category.users.each do |user|
+          user.albums.where(kind: 'portfolio').each do |album|
+            @albums << album
+          end
+        end
+      end
+      @albums.sort! { |a, b|  b.impressions_count <=> a.impressions_count }
     else
       @albums = Album.where(:category_id => @category.id, :kind => "portfolio")
     end
