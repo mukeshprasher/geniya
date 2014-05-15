bbf7c39fdcfea79648ba9aa9c8cf897152f90ffb
Neeraj Singh <neeraj.singh@60degree.com>
2014-05-15 19:02:15 +0530

    enter shift key press

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -68,7 +68,13 @@ $(function() {
 
   $('#comment_body').live("keypress", function(e) {
      var code = (e.keyCode ? e.keyCode : e.which);
-     if (code == 13) {
+     if (code == 13 && e.shiftKey) {
+        var content = this.value;
+        var caret = getCaret(this);
+        this.value = content.substring(0,caret)+"\n"+content.substring(carent,content.length-1);
+        e.stopPropagation();
+     }
+     else if (code == 13) {
         e.preventDefault();
         e.stopPropagation();
         $(this).closest('form').submit();
@@ -76,7 +82,42 @@ $(function() {
   });
  
  
+// $('#comment_body').keyup(function (event) {
+//       if (event.keyCode == 13 && event.shiftKey) {
+//           var content = this.value;
+//           var caret = getCaret(this);
+//           this.value = content.substring(0,caret)+"\n"+content.substring(carent,content.length-1);
+//           event.stopPropagation();
+//           
+//      }else if(event.keyCode == 13)
+//      {
+//          event.preventDefault();
+//          event.stopPropagation();
+//          $('#new_comment').submit();
+//      }
+//});
  
+  function getCaret(el) { 
+    if (el.selectionStart) { 
+      return el.selectionStart; 
+    } else if (document.selection) { 
+      el.focus(); 
+
+      var r = document.selection.createRange(); 
+      if (r == null) { 
+        return 0; 
+      } 
+
+      var re = el.createTextRange(), 
+          rc = re.duplicate(); 
+      re.moveToBookmark(r.getBookmark()); 
+      rc.setEndPoint('EndToStart', re); 
+
+      return rc.text.length; 
+    }  
+    return 0; 
+  }
+
  
 ////// for album Page slider 
  
