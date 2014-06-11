3d7ee61ee98e6356bc247ec3ea1175864c13e063
mukesh-avant-garde <mukesh.prashar@60degree.com>
2014-06-11 16:47:08 +0530

    plan differentiate for post jobs fix

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -17,7 +17,7 @@ class JobsController < ApplicationController
   # GET /jobs/new
   def new
     if current_user.plan == "trial"
-      redirect_to current_user, :alert => "Please Upgrade your Plan to deluxe for Post Jobs." 
+      redirect_to current_user, :alert => "Please Upgrade your Plan to Deluxe for Post Jobs." 
     else
       redirect_to new_organization_path, notice: 'Please create an organization to create a job.' unless current_user.organizations.any?
       @job = Job.new
