cde7142798082775edd6f12961db0cb91e324e9d
Neeraj Singh <neeraj.singh@60degree.com>
2014-06-13 18:23:16 +0530

    search for portfolio category pages

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -54,27 +54,59 @@ class PortfoliosController < ApplicationController
     @albums = Album.paginate(page: params[:page], per_page: 12).where(where_condition).order(order)
     @sql = Album.paginate(page: params[:page], per_page: 12).where(where_condition).order(order).to_sql
   end
+
   def category_index
+    where_condition = "kind = 'portfolio' AND category_id = "
     @category = Category.find(params[:id])
     @search_categories = Category.where(id: @category.id)
+    
+    where_condition = "kind = 'portfolio' AND category_id = #{@category.id} "
 
-    if !params[:q].nil?
-      q = "%#{params[:q]}%"
-      
-      @albums = (Album.where("category_id = ? and name   like ? and kind=?", @category.id, q, 'portfolio').order(impressions_count: :desc) | Album.where("category_id = ? and title like ? and kind=?", @category.id, q, 'portfolio').order(impressions_count: :desc) | Album.where("category_id = ? and description like ? and kind=?", @category.id, q, 'portfolio').order(impressions_count: :desc) ).uniq
-    elsif !params[:sc].nil?
-      @albums = Array.new
+    if params.has_key?(:q) and params[:q].present?
+      q = "'%#{params[:q]}%'"
+      where_condition += "and (name like #{q} or title like #{q}) "
+    end
+
+    if params.has_key?(:sc) and params[:sc].present?
+      user_ids = Array.new
       params[:sc].each do |sc_id|
         sub_category = SubCategory.find(sc_id.first)
         sub_category.users.each do |user|
-          user.albums.where(kind: 'portfolio').each do |album|
-            @albums << album
-          end
+          user_ids << user.id
         end
       end
-      @albums.sort! { |a, b|  b.impressions_count <=> a.impressions_count }
-    else
-      @albums = Album.paginate(page: params[:page], per_page: 12).where(:category_id => @category.id, :kind => "portfolio")
+
+      user_ids_for_sql = user_ids.join(',')
+
+      where_condition += "and user_id IN (#{user_ids_for_sql}) "
     end
+
+    if params.has_key?(:city) and params[:city].present?
+      city_name = params[:city].gsub(/[^A-Za-z]/, '').downcase
+      city = City.find_by(name: city_name)
+      where_condition += "and city_id = #{city.id} " unless city.nil?
+    end
+
+    if params.has_key?(:country) and params[:country].present?
+      where_condition += "and country_id = #{params[:country]} "
+    end
+
+    if params.has_key?(:order) and params[:order].present?
+      if params[:order] == 'recent'
+        order = "created_at desc"
+      elsif params[:order] == 'discussed'
+        order = "comment_count desc"
+      elsif params[:order] == 'liked'
+        order = "like_count desc"
+      elsif params[:order] == 'viewed'
+        order = "impressions_count desc"
+      else
+        order = "impressions_count desc"
+      end
+    else
+      order = "impressions_count desc"
+    end
+
+    @albums = Album.paginate(page: params[:page], per_page: 12).where(where_condition).order(order)
   end
 end
