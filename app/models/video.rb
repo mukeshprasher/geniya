99ad0a8f26ada60b88e1eeb980752c16246999a9
mukesh-avant-garde <mukesh.prashar@60degree.com>
2014-06-18 15:41:21 +0530

    fix impression count for video

--- app/models/video.rb
+++ app/models/video.rb
@@ -17,7 +17,8 @@ class Video < ActiveRecord::Base
 
   acts_as_likeable
   acts_as_commentable 
-  is_impressionable counter_cache: true
+  #is_impressionable counter_cache: true
+  is_impressionable :unique => :user_id, :counter_cache => true
   extend FriendlyId
   friendly_id :slug_adds, use: [:slugged, :finders]  
 
