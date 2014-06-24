889513c4ad2e8d51c2ee346726ba3f4cb6b4d98b
mukesh-avant-garde <mukesh.prashar@60degree.com>
2014-06-24 16:09:09 +0530

    hide category fields for visitor plan

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -60,10 +60,10 @@ class EducationsController < ApplicationController
   # DELETE /educations/1.json
   def destroy
     @education.destroy
-    respond_to do |format|
-      format.html { redirect_to educations_url }
-      format.json { head :no_content }
-    end
+#    respond_to do |format|
+#      format.html { redirect_to educations_url }
+#      format.json { head :no_content }
+#    end
   end
 
   private
