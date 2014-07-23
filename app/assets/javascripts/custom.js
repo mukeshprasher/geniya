10454dfa33ccc613f0506940d3818d5570165d4b
Neeraj Singh <neeraj.singh@60degree.com>
2014-07-23 18:03:45 +0530

    serach image on text-box on search text-box

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -263,10 +263,23 @@ $(function() {
                   event.stopPropagation();
                  }
                }
-               
-               
-               //event.preventDefault();
-              });              
+              });
+              
+              
+              $('#profile-srch-btn').click(function(event) {
+                 var user_slug = $('.profile-data.cur').attr( "data" );
+                 var srch_txt = $('#srch_profile').val();
+                 var complete_txt = $.trim(srch_txt);
+                 if (user_slug == complete_txt)
+                 {
+                    window.location.href = "/users/"+user_slug;
+                 }
+                 else
+                 {
+                  $('#no_srch_content').show('slow');
+                  event.stopPropagation();
+                 }
+              });                  
               });
             }
           }
