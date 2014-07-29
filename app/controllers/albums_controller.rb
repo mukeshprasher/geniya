37bfa6c6b535b6aa5997c6ac514f06d0164e311c
Neeraj Singh <neeraj.singh@60degree.com>
2014-07-29 16:28:37 +0530

    fix error

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -21,10 +21,10 @@ class AlbumsController < ApplicationController
     @user_profile_pics = @user.uploads.where(upload_type: 'profile')
     impressionist @album, '', unique: [:user_id] if current_user
     @likers = @album.likers(User)
-    @video = current_user.videos.build
     @comments = @album.comment_threads.where('parent_id IS NULL').order('created_at desc')
     if signed_in?
       @new_comment = Comment.build_from(@album, current_user.id, "")
+      @video = current_user.videos.build
     end
   end
 
