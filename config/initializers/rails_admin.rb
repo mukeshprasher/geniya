042c00bc289248fe63acae6711d46c5ccf9021b0
Neeraj Singh <neeraj.singh@60degree.com>
2014-08-20 17:35:21 +0530

    exclude the unwanted models from rails admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -8,7 +8,7 @@ RailsAdmin.config do |config|
   # end
   # config.current_user_method(&:current_user)
   config.current_user_method(&:admin_user)
-  config.excluded_models = ["Like", "Follow", "Mention", "Message", "Conversation"]
+  config.excluded_models = ["Like", "Follow", "Mention", "Message", "Conversation", "Activity", "DevelopmentLog", "Rate", "RatingCache", "Response", "Relationship", "Share", "PaymentSubscription", "Payment", "Chat", "FeedbackReply", "Connection", "Category"]
 
   ## == Cancan ==
   # config.authorize_with :cancan
