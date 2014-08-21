7bc94c2cb5fca9b8c4bd06d2085a3f2b825505e4
Neeraj Singh <neeraj.singh@60degree.com>
2014-08-21 17:30:23 +0530

    fix the change login process

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -109,10 +109,12 @@ class UsersController < ApplicationController
   end
  
   def change_login
-#    if @user.parent_id == current_user.id || @user.id == current_user.parent_id
-    sign_in @user
-    redirect_to @user
-#    end
+    if @user.parent_id == current_user.id || @user.id == current_user.parent_id || @user.parent_id == current_user.parent_id
+      sign_in @user
+      redirect_to @user
+    else
+      redirect_to current_user
+    end
   end
   
   def edit
