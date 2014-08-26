2e2257876975995d359c5293115e01ad436ec72b
Neeraj Singh <neeraj.singh@60degree.com>
2014-08-26 14:53:53 +0530

    fix qa error

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -9,6 +9,6 @@ class Feedback < ActiveRecord::Base
   
     
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, length: { minimum: 5,  maximum: 50}
   
 end
