6352254cbb1cd771383d77013213e9b806cc6329
Neeraj Singh <neeraj.singh@60degree.com>
2014-08-28 18:37:27 +0530

    facebook contacts

--- config/initializers/omnicontacts.rb
+++ config/initializers/omnicontacts.rb
@@ -4,7 +4,7 @@ Rails.application.middleware.use OmniContacts::Builder do
   importer :gmail, "351768923449-u963j49k8vjdg7960lub7q3v22vgittn.apps.googleusercontent.com", "S1TEvcdKXH9BL9eu806m7bNr", {:redirect_path => "/invites/gmail/contact_callback"}
   importer :yahoo, "consumer_id", "consumer_secret", {:callback_path => '/callback'}
   importer :hotmail, "client_id", "client_secret"
-  importer :facebook, "client_id", "client_secret"
+  importer :facebook, "1525309957687470", "adf1333680fef8c2cab6418ae06311bf"
 end
 
 
