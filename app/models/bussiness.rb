9b58584f4ff3aae915fc97c50d677c0d4f45c5cb
mukesh-avant-garde <mukesh.prashar@60degree.com>
2014-08-28 18:25:36 +0530

    fix admin for business and organization

--- app/models/bussiness.rb
+++ app/models/bussiness.rb
@@ -1,12 +1,12 @@
 class Bussiness < ActiveRecord::Base
   belongs_to :user
-#  has_attached_file :file_attachment, :styles => { :large => "600x600>", :medium => "300x300>"}, :default_url => "/assets/missing-image-geniya.png", :hash_secret => "geniyaAttach", :path => ":rails_root/public/system/bussinesscovers/:id_:style.:extension", :url => "/system/bussinesscovers/:id_:style.:extension",
-#                        :storage        => :s3                                                 ,
-#                      :s3_credentials => {:bucket            => 'geniyanet',
-#                                          :access_key_id     => 'AKIAIUP2GZ5XEI6BZW3A',
-#                                          :secret_access_key => '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'},
-#                      :s3_protocol    => "http"                                             ,
-#                      :bucket => "geniyanet"
+  has_attached_file :file_attachment, :styles => { :large => "600x600>", :medium => "300x300>"}, :default_url => "/assets/missing-image-geniya.png", :hash_secret => "geniyaAttach", :path => ":rails_root/public/system/bussinesscovers/:id_:style.:extension", :url => "/system/bussinesscovers/:id_:style.:extension",
+                        :storage        => :s3                                                 ,
+                      :s3_credentials => {:bucket            => 'geniyanet',
+                                          :access_key_id     => 'AKIAIUP2GZ5XEI6BZW3A',
+                                          :secret_access_key => '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'},
+                      :s3_protocol    => "http"                                             ,
+                      :bucket => "geniyanet"
 #  crop_attached_file :file_attachment , :aspect => "600:600"
 #  validates_attachment :file_attachment,
 #  presence: true, 
