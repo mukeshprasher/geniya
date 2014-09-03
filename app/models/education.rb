f20e6624a8fab229771dc0c8e5d25f92f1e61a12
Neeraj Singh <neeraj.singh@60degree.com>
2014-09-03 18:11:35 +0530

    corect edu model

--- app/models/education.rb
+++ app/models/education.rb
@@ -1,6 +1,6 @@
 class Education < ActiveRecord::Base
   belongs_to :user
 
-  validates :institute_name,  presence: true, length: { minimum: 4,  maximum: 80 }
-  validates :course_name,  presence: true, length: { minimum: 4,  maximum: 80 }
+  validates :institute_name,  presence: true, length: { minimum: 1,  maximum: 80 }
+  validates :course_name,  presence: true, length: { minimum: 1,  maximum: 80 }
 end
